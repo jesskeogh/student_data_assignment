0a737cc733969c74aa1dd6b262471ef1abd75285
Jessica Keogh <Jessica.Keogh@rolls-roycemotorcars.com>
2025-12-08 13:15:24 +0000

    assignment basics - average grade - average attendance - no of passes - grade distribution

--- create_db.py
+++ create_db.py
@@ -1,21 +1,24 @@
 # create dataframe from csv file - make function?
+# import pandas and SQLite
 import pandas as pd
-df = pd.read_csv('student_grades.csv')
-
-# import SQLite
 import sqlite3
-conn = sqlite3.connect(r"student_grades.db")
+
+def create_db():
+    df = pd.read_csv('student_grades.csv')
+    conn = sqlite3.connect("student_grades.db")
+    with conn:
+        df.to_sql("student_data", conn, if_exists="replace", index=False)
+    conn.close()
+
+if __name__ == '__main__':
+    create_db()
+
+#create_db()
+
+#c = conn.cursor()
+#for row in c.execute('SELECT * FROM student_data'):
+#    print(row)
 
 #import as a function
 #def db_connection():
   #  return sqlite3.connect('data/student_grades.db')
-
-def create_db():
-    with conn:
-        df.to_sql("student_data", conn, if_exists="replace", index=False)
-
-create_db()
-
-c = conn.cursor()
-for row in c.execute('SELECT * FROM student_data'):
-    print(row)
