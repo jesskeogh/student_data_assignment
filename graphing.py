b2b98af3d108cc3483fff31234004d7100c5c436
Jessica Keogh <Jessica.Keogh@rolls-roycemotorcars.com>
2026-01-06 11:25:59 +0000

    Adding correlation graph for attendance vs grade

--- graphing.py
+++ graphing.py
@@ -29,6 +29,35 @@ def plot_grade_distribution_pie():
     plt.ylabel("")  # hide y-label
     plt.show()
 
+import sqlite3
+import pandas as pd
+import numpy as np
+
+def grade_against_attendance():
+    conn = sqlite3.connect('student_grades.db')
+    df = pd.read_sql_query("SELECT grade, attendance FROM student_data", conn)
+    conn.close()
+    return df
+
+# load data
+df = grade_against_attendance()
+
+# get X and Y values
+x = df['attendance']
+y = df['grade']
+
+# create scatter graph
+plt.scatter(x, y, alpha=0.5) # alpha is the density of the data points
+plt.xlabel("attendance (%)")
+plt.ylabel("Grade")
+plt.title("Correlation between Attendance and Grade")
+
+# Add trendline
+m, b = np.polyfit(x, y, 1)
+plt.plot(x, m*x + b, color='red')
+
+plt.show()
+
 if __name__ == "__main__":
     plot_grade_distribution()
     plot_grade_distribution_pie()
